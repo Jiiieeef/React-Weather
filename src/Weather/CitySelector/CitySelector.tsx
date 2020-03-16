000eb5b83f108ff469e9a5a8b21aaf9c9bf87c66
Jean-Franck Ballet <ballet.jean.franck@gmail.com>
2020-03-16 22:12:59 +0100

    Add a placeholder to city selector

--- src/Weather/CitySelector/CitySelector.tsx
+++ src/Weather/CitySelector/CitySelector.tsx
@@ -10,15 +10,25 @@ interface CitySelectorProps {
 }
 
 class CitySelector extends Component<CitySelectorProps> {
+  state = {
+    selectedCityId: '0'
+  }
+
   cityOption(city: any) {
     return (<option key={city.id} value={city.id}>{city.nm}</option>);
   }
 
   citiesSelect = () => {
-    return (<select onChange={this.onCityChange} placeholder="Select a city">{cities.map(this.cityOption.bind(this))}</select>);
+    return (
+      <select onChange={this.onCityChange} value={this.state.selectedCityId}>
+        <option value='0' disabled>Select a city</option>
+        {cities.map(this.cityOption.bind(this))}
+      </select>
+      );
   }
 
   onCityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    this.setState({selectedCityId: event.target.value});
     this.props.onCityChange(cities.find((city: City) => +city.id === +event.target.value));
   }
 
